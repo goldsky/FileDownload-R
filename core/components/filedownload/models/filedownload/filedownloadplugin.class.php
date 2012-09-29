3612b6e9f3e2c3a96a75079a6174d0958c7e59a2
goldsky <goldsky@fastmail.fm>
2012-09-29 18:41:19 +0700

    Add references to some properties

--- core/components/filedownload/models/filedownload/filedownloadplugin.class.php
+++ core/components/filedownload/models/filedownload/filedownloadplugin.class.php
@@ -10,8 +10,8 @@ class FileDownloadPlugin {
     private $_appliedEvents = array();
     private $_allEvents = array();
 
-    public function __construct(FileDownload $fileDownload) {
-        $this->modx = & $fileDownload->modx;
+    public function __construct(FileDownload &$fileDownload) {
+        $this->modx = &$fileDownload->modx;
         $this->configs = $fileDownload->configs;
         $this->fileDownload = $fileDownload;
         $this->_allEvents = include $this->configs['basePath'] . 'plugins/filedownloadplugin.events.php';
@@ -33,6 +33,38 @@ class FileDownloadPlugin {
         }
     }
 
+    /**
+     * Set custom property for the plugin in the run time
+     * @param   string  $key    key
+     * @param   string  $val    value
+     * @return  void
+     */
+    public function setProperty($key, $val) {
+        $this->configs = array_merge($this->configs, array($key => $val));
+    }
+
+    /**
+     * Set custom properties for the plugin in the run time in an array of
+     * key => value pairings
+     * @param   array   $array  array of the properties
+     * @return  void
+     */
+    public function setProperties($array = array()) {
+        if (is_array($array)) {
+            foreach ($array as $key => $val) {
+                $this->setProperty($key, $val);
+            }
+        }
+    }
+
+    public function getProperty($key) {
+        return $this->configs[$key];
+    }
+
+    public function getProperties() {
+        return $this->configs;
+    }
+
     public function getAllEvents() {
         return $this->_allEvents;
     }
@@ -51,7 +83,7 @@ class FileDownloadPlugin {
      * @param   boolean         $toString   return the results as string instead
      * @return  boolean|array   FALSE | plugin's output array
      */
-    public function getPlugins($eventName, $toString=FALSE) {
+    public function getPlugins($eventName, $toString = FALSE) {
         $this->_event = $eventName;
         $output = array();
         if (empty($this->_appliedEvents[$eventName]) ||
@@ -76,60 +108,28 @@ class FileDownloadPlugin {
         return $output;
     }
 
-    /**
-     * Set custom property for the plugin in the run time
-     * @param   string  $key    key
-     * @param   string  $val    value
-     * @return  void
-     */
-    public function setProperty($key, $val) {
-        $this->configs = array_merge($this->configs, array($key => $val));
-    }
-
-    /**
-     * Set custom properties for the plugin in the run time in an array of
-     * key => value pairings
-     * @param   array   $array  array of the properties
-     * @return  void
-     */
-    public function setProperties($array=array()) {
-        if (is_array($array)) {
-            foreach ($array as $key => $val) {
-                $this->setProperty($key, $val);
-            }
-        }
-    }
-
-    public function getProperty($key) {
-        return $this->configs[$key];
-    }
-
-    public function getProperties() {
-        return $this->configs;
-    }
-
-    private function _loadPlugin ($plugin) {
+    private function _loadPlugin($plugin) {
         $pluginName = $plugin['name'];
         $success = FALSE;
-        if ($snippet = $this->modx->getObject('modSnippet',array('name' => $pluginName))) {
+        if ($snippet = $this->modx->getObject('modSnippet', array('name' => $pluginName))) {
             /* custom snippet plugin */
             $properties = $this->configs;
-            $properties['fileDownload'] =& $this->fileDownload;
-            $properties['plugin'] =& $this;
-            $properties['errors'] =& $this->errors;
+            $properties['fileDownload'] = &$this->fileDownload;
+            $properties['plugin'] = &$this;
+            $properties['errors'] = &$this->errors;
             $success = $snippet->process($properties);
         } else {
-            $plugin =& $this;
+            $plugin = &$this;
             $modx = $this->modx;
             $fileDownload = $this->fileDownload;
 
             /* search for a file-based plugin */
-            $this->modx->parser->processElementTags('',$pluginName,true,true);
+            $this->modx->parser->processElementTags('', $pluginName, true, true);
             if (file_exists($pluginName)) {
                 $success = $this->_loadFileBasedPlugin($pluginName);
             } else {
                 /* no plugin found */
-                $this->modx->log(modX::LOG_LEVEL_ERROR,'[FileDownload] Could not find plugin "'.$pluginName.'".');
+                $this->modx->log(modX::LOG_LEVEL_ERROR, '[FileDownload] Could not find plugin "' . $pluginName . '".');
                 $success = FALSE;
             }
         }
@@ -145,16 +145,17 @@ class FileDownloadPlugin {
      */
     private function _loadFileBasedPlugin($path) {
         $scriptProperties = $this->configs;
-        $fileDownload =& $this->fileDownload;
-        $plugin =& $this;
-        $errors =& $this->errors;
-        $modx =& $this->modx;
+        $fileDownload = &$this->fileDownload;
+        $plugin = &$this;
+        $errors = &$this->errors;
+        $modx = &$this->modx;
         $success = false;
         try {
             $success = include $path;
         } catch (Exception $e) {
-            $this->modx->log(modX::LOG_LEVEL_ERROR,'[FileDownload] '.$e->getMessage());
+            $this->modx->log(modX::LOG_LEVEL_ERROR, '[FileDownload] ' . $e->getMessage());
         }
         return $success;
     }
+
 }
